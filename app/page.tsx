46dddc9517bffaa04dcdc59ff041aea5b23773a7
Hendra Arfianto <hendra.arfianto@gmail.com>
2024-07-09 00:27:21 +0700

    Card component
    Creating a card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import Card from "@/components/ui/card";
 import WaterWaveWrapper from "@/components/visualEffects/water-wave-wrapper";
 
 export default function Home() {
@@ -10,7 +10,13 @@ export default function Home() {
       perturbance="3"
       resolution="2048"
     >
-      {() => <div className="h-screen"></div>}
+      {() => (
+        <div className="w-full p-20">
+          <div className="max-w-2xl mx-auto">
+            <Card title="Ui Components">...</Card>
+          </div>
+        </div>
+      )}
     </WaterWaveWrapper>
   );
 }
