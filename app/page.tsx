781531bc57ab7f810ff041e9dfd23c22f9d4e913
Hendra Arfianto <hendra.arfianto@gmail.com>
2024-07-08 12:43:28 +0700

    Clsx and Tailwind-merge
    Added clsx and tailwind-merge module

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import { clsx } from "clsx";
+import { cn } from "@/lib/utils";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl font-bold text-red-600">Hello my friends</h1>
+      <h1 className={cn("italic", "text-yellow-400", true && "underline")}>
+        Hello my friends
+      </h1>
     </div>
   );
 }
