40b415ab2a7623cc2160ee88780ef84151dd8ceb
Hendra Arfianto <hendra.arfianto@gmail.com>
2024-07-14 20:41:21 +0700

    Add Fancy button
    Added fancy button component

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,12 @@
 
 import Button from "@/components/ui/button";
 import Card from "@/components/ui/card";
+import FancyButton from "@/components/ui/fancy-button";
 import Input from "@/components/ui/input";
 import Profile from "@/components/ui/profile";
 import TextArea from "@/components/ui/text-area";
 import WaterWaveWrapper from "@/components/visualEffects/water-wave-wrapper";
-import { FaUser } from "react-icons/fa";
+import { FaArrowRight, FaUser } from "react-icons/fa";
 import { GoHomeFill } from "react-icons/go";
 
 export default function Home() {
@@ -37,6 +38,9 @@ export default function Home() {
               <TextArea placeholder="Full Name" />
               <TextArea placeholder="Full Name" icon={<FaUser />} />
               <Profile />
+              <div className="w-350px">
+                <FancyButton text="Contact Us" icon={<FaArrowRight />} />
+              </div>
             </Card>
           </div>
         </div>
