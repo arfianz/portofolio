6c29c94dbf9d9ae43091e4644dd7af40951f938b
Hendra Arfianto <hendra.arfianto@gmail.com>
2024-07-14 23:40:18 +0700

    Create a Landing page
    Creating a new landing page, replacing previous one

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,7 @@
 "use client";
 
-import Button from "@/components/ui/button";
-import Card from "@/components/ui/card";
-import FancyButton from "@/components/ui/fancy-button";
-import Input from "@/components/ui/input";
-import LiveClock from "@/components/ui/live-clock";
-import Profile from "@/components/ui/profile";
-import ScrollDown from "@/components/ui/scroll-down";
-import TextArea from "@/components/ui/text-area";
-import MagneticWrapper from "@/components/visualEffects/magnetic-wrapper";
 import WaterWaveWrapper from "@/components/visualEffects/water-wave-wrapper";
-import { FaArrowRight, FaUser } from "react-icons/fa";
-import { GoHomeFill } from "react-icons/go";
+import LandingSection from "@/sections/landing";
 
 export default function Home() {
   return (
@@ -22,33 +12,9 @@ export default function Home() {
       resolution="2048"
     >
       {() => (
-        <div className="w-full p-20">
-          <div className="max-w-2xl mx-auto">
-            <Card title="Ui Components">
-              <div className="grid grid-cols-4">
-                <Button>Basic Button</Button>
-                <Button>
-                  <GoHomeFill />
-                  Home Button
-                </Button>
-                <Button>
-                  <FaUser />
-                </Button>
-                <Button link="https://www.google.com">Google</Button>
-              </div>
-              <Input type="text" placeholder="Full Name" />
-              <Input type="text" placeholder="Full Name" icon={<FaUser />} />
-              <TextArea placeholder="Full Name" />
-              <TextArea placeholder="Full Name" icon={<FaUser />} />
-              <Profile />
-              <MagneticWrapper className="w-350px">
-                <FancyButton text="Contact Us" icon={<FaArrowRight />} />
-              </MagneticWrapper>
-              <LiveClock timeZone="Asia/Jakarta" />
-              <MagneticWrapper>
-                <ScrollDown />
-              </MagneticWrapper>
-            </Card>
+        <div>
+          <div>
+            <LandingSection />
           </div>
         </div>
       )}
--- components/ui/profile.tsx
+++ components/ui/profile.tsx
@@ -10,6 +10,7 @@ export default function Profile() {
           src={MyImage}
           alt="Arfian Dwi"
           className="w-[95px] h-{95px] border-[0.2vw] border-blue-cosmos rounded-full object-cover"
+          priority={true}
         />
         {/*Online*/}
         <div className="w-3 h-3 rounded-full bg-green-benzol border border-blue-cosmos absolute right-0 bottom-5"></div>
